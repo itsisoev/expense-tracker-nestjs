66ccc23377c608bb48b5c4134e201fb318beeb2a
Daler <isoev9733@gmail.com>
2024-08-15 15:18:18 +0500

    add transaction

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,20 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Category } from 'src/category/entities/category.entity';
+import { User } from 'src/user/entities/user.entity';
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  amount: number;
+
+  @IsString()
+  type: 'expense' | 'income';
+
+  @IsNotEmpty()
+  category: Category;
+
+  //@IsNotEmpty()
+  user: User;
+}
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>,
+  ) {}
+
+  async createTransaction(
+    createTransactionDto: CreateTransactionDto,
+    id: number,
+  ) {
+    const newTransaction = this.transactionRepository.create({
+      title: createTransactionDto.title,
+      amount: createTransactionDto.amount,
+      type: createTransactionDto.type,
+      category: { id: +createTransactionDto.category },
+      user: { id },
+    });
+
+    return await this.transactionRepository.save(newTransaction);
   }
 
-  findAll() {
-    return `This action returns all transaction`;
+  async findTransactions(id: number) {
+    return await this.transactionRepository.find({
+      where: {
+        user: { id },
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findTransaction(id: number) {
+    const transaction = await this.transactionRepository.findOne({
+      where: { id },
+      relations: ['user', 'category'],
+    });
+
+    if (!transaction) throw new NotFoundException('Transaction not found');
+    return transaction;
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async updateTransaction(
+    id: number,
+    updateTransactionDto: UpdateTransactionDto,
+  ) {
+    const transaction = await this.transactionRepository.preload({
+      id,
+      ...updateTransactionDto,
+    });
+
+    if (!transaction) throw new NotFoundException('Transaction not found');
+
+    return await this.transactionRepository.save(transaction);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async removeTransaction(id: number) {
+    const transaction = await this.findTransaction(id);
+
+    await this.transactionRepository.remove(transaction);
+    return transaction;
+  }
+
+  async findTransactionsWithPagination(
+    id: number,
+    page: number,
+    limit: number,
+  ) {
+    return await this.transactionRepository.find({
+      where: {
+        user: { id },
+      },
+      relations: ['user', 'category'],
+      order: {
+        createdAt: 'DESC',
+      },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
   }
 }
